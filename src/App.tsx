0b5cdd2014d0c3396d89e0fd3a27b5401e374d74
Hugo Alves Varella <hugovarellaa@gmail.com>
2021-10-12 08:59:30 -0300

    .
    .

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import * as C from "./App.styles";
+
+export const App = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>
+          Sistema Financeiro
+        </C.HeaderText>
+      </C.Header>
+      <C.Body>
+          {/* área de informação */}
+          {/* área de inserção */}
+          {/* tabela de itens */}
+        </C.Body>
+    </C.Container>
+  )
+};
