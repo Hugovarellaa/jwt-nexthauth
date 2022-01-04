a2842b214f8ae4602595ad5edfccc2e35e838ef8
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-01-04 17:44:46 -0300

    Delete expense-tracker directory

--- expense-tracker/src/components/InputArea/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-    background: #fff;
-    box-shadow: 0px 0px 5px #ccc;
-    border-radius: 10px;
-    padding: 20px;
-    margin-top: 20px;
-`
--- expense-tracker/src/types/item.ts
+++
@@ -1,6 +0,0 @@
-export type Item = {
-  date: Date;
-  title: string;
-  value: number;
-  category: string;
-};
